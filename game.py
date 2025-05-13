ed51da4e917357e3776d816a29a907734fc0fca3
Tobias Gärdebro <tubbeg@gmail.com>
2025-05-13 22:58:57 +0200

    Added controls. Player can now move sprite

--- game.py
+++ game.py
@@ -1,13 +1,45 @@
 import pyxel
+from enum import Enum
 
+
+class Direction(Enum):
+    NOWHERE = 1
+    LEFT = 2
+    RIGHT = 3
+
+def is_moving_left():
+    return pyxel.btn(pyxel.KEY_A)
+
+def is_moving_right():
+    return pyxel.btn(pyxel.KEY_D)
+
+def get_direction():
+    if is_moving_left():
+        return Direction.LEFT
+    if is_moving_right():
+        return Direction.RIGHT
+    return Direction.NOWHERE
+
+sprite_pos = (0,0)
+
+def move_sprite(dir):
+    global sprite_pos
+    x,y = sprite_pos
+    if dir is Direction.LEFT:
+        sprite_pos = (x - 1, y)
+    if dir is Direction.RIGHT:
+        sprite_pos = (x + 1, y)
 
 
 def update():
-    pass
+    dir = get_direction ()
+    move_sprite (dir)
 
 def draw():
+    global sprite_pos
+    x,y = sprite_pos
     pyxel.cls(0)
-    pyxel.blt(0, 0, 0, 0, 0, 16, 16)
+    pyxel.blt(x, y, 0, 0, 0, 16, 16)
     #pyxel.rect(x, 0, 8, 8, 9)
 
 
@@ -18,3 +50,6 @@ def run_game ():
 
 
 run_game()
+
+if __name__ == "__main__":
+    run_game()
